59d5d32fa8b29be150b2e3c7083804f0a4fe3ff7
Henk Muller <henk@xmos.com>
2011-09-21 17:18:45 +0100

    added summary file, and corrected documentation in .h file

--- module_adat_rx/src/adat_rx.h
+++ module_adat_rx/src/adat_rx.h
@@ -9,11 +9,11 @@
  *  \param p ADAT port - should be 1-bit and clocked at 100MHz
  *  \param oChan channel on which decoded samples are output
  *
- *  The function will return if it cannot lock onto a 44,100/48,000 Hz
+ *  The function will return if it cannot lock onto a 48,000 Hz
  *  signal. Normally the 48000 function is called in a while(1) loop. If
  *  both 44,100 and 48,000 need to be supported, they should be called in
  *  sequence in a while(1) loop. Note that the functions are large, and
- *  that 44,100 should not be called if it does not need to be supported. 
+ *  that 44,100 should not be called if 44.1 KHz does not need to be supported. 
  **/
 void adatReceiver48000(buffered in port:32 p, chanend oChan);
 
@@ -22,11 +22,11 @@ void adatReceiver48000(buffered in port:32 p, chanend oChan);
  *  \param p ADAT port - should be 1-bit and clocked at 100MHz
  *  \param oChan channel on which decoded samples are output
  *
- *  The function will return if it cannot lock onto a 44,100/48,000 Hz
- *  signal. Normally the 48000 function is called in a while(1) loop. If
+ *  The function will return if it cannot lock onto a 44,100 Hz
+ *  signal. Normally the 44,100 function is called in a while(1) loop. If
  *  both 44,100 and 48,000 need to be supported, they should be called in
  *  sequence in a while(1) loop. Note that the functions are large, and
- *  that 44,100 should not be called if it does not need to be supported. 
+ *  that 48,000 should not be called if 48 Khz does not need to be supported. 
  **/
 
 void adatReceiver44100(buffered in port:32 p, chanend oChan);
